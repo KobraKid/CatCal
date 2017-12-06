2866159640456dbea53e01f5829d2df61eba19b6
Michael <michael@Michaels-iMac.local>
2017-12-05 23:52:07 -0600

    Initial working app.

---
+++ CatCal/CalendarCell.swift
@@ -0,0 +1,32 @@
+//
+//  CalendarCell.swift
+//  CatCal
+//
+//  Created by Michael on 12/5/17.
+//  Copyright © 2017 DotDev. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+ 
+    var textLabel: UILabel!
+    let padding: CGFloat = 10.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        textLabel = UILabel(frame: CGRect(x: padding, y: 0, width: frame.size.width - CGFloat(2 * padding), height: frame.size.height))
+        textLabel.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        textLabel.textAlignment = .natural
+        contentView.addSubview(textLabel)
+    }
+    
+    /**
+     This initializer is called instead of init(frame:) when the class gets initialized from a storyboard or a xib file. That'll never be the case here, but we still need to provide init(coder:). 
+     */
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- CatCal/ViewController.swift
+++ CatCal/ViewController.swift
@@ -6,20 +6,159 @@
 //  Copyright © 2017 DotDev. All rights reserved.
 //
 
+import GoogleAPIClientForREST
+import GoogleSignIn
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLRAuthScopeCalendar/* Readonly */]
+    
+    // Google API related
+    private let service = GTLRCalendarService()
+    let signInButton = GIDSignInButton()
+    let output = UITextView()
+    
+    var calendarToAccess = "u.northwestern.edu_uuh3sk34il40hq330fm95jiaic@group.calendar.google.com"
+    
+    // Layout related
+    let numberOfCells = 24
+    let cellID = "CalendarCell"
+    var colorCounter = 0
+    let navHeight: CGFloat = 64.0
+    var events: [String: String] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.collectionView!.delegate = self
+        self.collectionView!.register(CalendarCell.self, forCellWithReuseIdentifier: cellID)
+        
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // Add the sign-in button.
+        view.addSubview(signInButton)
+        
+        // Add a UITextView to display output.
+        output.frame = CGRect(x: 0.0, y: navHeight, width: view.bounds.width, height: view.bounds.height - navHeight)
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        output.isHidden = true
+        output.backgroundColor = .lightGray
+        view.addSubview(output);
+    }
+    
+    @IBAction func refreshView(_ sender: Any) {
+        fetchEvents()
+        self.collectionView!.reloadData()
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            service.authorizer = nil
+        } else {
+            signInButton.isHidden = true
+            // output.isHidden = false
+            service.authorizer = user.authentication.fetcherAuthorizer()
+            fetchEvents()
+        }
+    }
+    
+    // Construct a query and get a list of upcoming events from the user calendar
+    func fetchEvents() {
+        let query = GTLRCalendarQuery_EventsList.query(withCalendarId: calendarToAccess)
+        query.maxResults = 10
+        query.timeMin = GTLRDateTime(date: Date())
+        query.singleEvents = true
+        query.orderBy = kGTLRCalendarOrderByStartTime
+        service.executeQuery(
+            query,
+            delegate: self,
+            didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
+    }
+    
+    // Display the start dates and event summaries in the UITextView
+    @objc func displayResultWithTicket(
+        ticket: GTLRServiceTicket,
+        finishedWithObject response : GTLRCalendar_Events,
+        error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        var outputText = ""
+        self.events.removeAll()
+        if let events = response.items, !events.isEmpty {
+            for event in events {
+                let start = event.start!.dateTime ?? event.start!.date!
+                let startString = DateFormatter.localizedString(
+                    from: start.date,
+                    dateStyle: .short,
+                    timeStyle: .short)
+                outputText += "\(startString) - \(event.summary!)\n"
+                self.events[event.identifier!] = outputText
+            }
+        } else {
+            outputText = "No upcoming events found."
+        }
+        output.text = outputText
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
     }
-
-
+    
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    // Tell the CollectionView how many cells you need
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    // Give the CollectionView the cell you want it to display at indexPath
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellID, for: indexPath) as! CalendarCell
+        cell.textLabel.text = events.popFirst()?.value
+        cell.backgroundColor = colorCounter % 2 == 0 ? UIColor.gray : UIColor.lightGray
+        colorCounter = colorCounter + 1
+        return cell
+    }
+    
+    // Tell the CollectionView what size to make the cell at indexPath
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath:
+        IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: (view.frame.height - navHeight) / CGFloat(numberOfCells))
+    }
+    
+    // Handle what happens when you tap on a cell
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
+}

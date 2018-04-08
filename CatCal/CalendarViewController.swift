fff06af3b321facfe848ef2fe1a0dc06ea976c12
Yintian <yintianzhan2021@u.northwestern.edu>
2018-04-08 00:41:16 -0500

    partially updated monthly view

--- CatCal/CalendarViewController.swift
+++ CatCal/CalendarViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var month: UILabel!
     let formatter = DateFormatter()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +22,26 @@ class CalendarViewController: UIViewController {
 
 }
 
-extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
-    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        
+func handleCelltextColor(view: JTAppleCell?, cellState: CellState){
+    guard let validCell = view as? CustomCell else { return }
+    
+    if cellState.dateBelongsTo == .thisMonth {
+        validCell.dateLabel.textColor = UIColor.white
+    } else {
+        validCell.dateLabel.textColor = UIColor.gray
     }
     
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate,JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
     
+    }
     
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         cell.dateLabel.text = cellState.text
+        handleCelltextColor(view: cell, cellState: cellState)
         return cell
     }
     
@@ -36,11 +49,20 @@ extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
         formatter.dateFormat = "yyyy MM DD"
         formatter.timeZone = Calendar.current.timeZone
         formatter.locale = Calendar.current.locale
-        let startDate = formatter.date(from: "2018 01 01")!
+        let startDate = formatter.date(from: "2017 03 01")!
         let endDate = formatter.date(from: "2018 12 31")!
         let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
         return parameters
     }
     
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        let date = visibleDates.monthDates.first!.date
+        
+        formatter.dateFormat = "yyyy"
+        year.text = formatter.string(from: date)
+        
+        formatter.dateFormat = "MMMM"
+        Month.text = formatter.string(from: date)
+    }
     
 }
